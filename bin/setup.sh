6039e8fc15ab32df00c06a6cf06749f15892af70
Les Matheson <les.matheson@gmail.com>
2022-08-06 12:06:36 +0000

    Sync (auto)

---
+++ bin/setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# setup.sh for taskrc-kit
+
+die() {
+    echo "ERROR: $@" >&2
+    exit 1
+}
+
+canonpath() {
+    ( cd -L -- "$(dirname -- $0)"; echo "$(pwd -P)/$(basename -- $0)" )
+}
+
+Script=$(canonpath "$0")
+Scriptdir=$(dirname -- "$Script")
+inode() {
+    ( command ls -i "$1" | command awk '{print $1}') 2>/dev/null
+}
+
+is_on_path() {
+    local tgt_dir="$1"
+    [[ -z $tgt_dir ]] && { true; return; }
+    local vv=( $(echo "${PATH}" | tr ':' '\n') )
+    for v in ${vv[@]}; do
+        if [[ $tgt_dir == $v ]]; then
+            return
+        fi
+    done
+    false
+}
+
+path_fixup() {
+    # Add ~/.local/bin to the PATH if it's not already.  Modify
+    # either .bash_profile or .profile honoring bash startup rules.
+    local tgt_dir="$1"
+    if is_on_path "${tgt_dir}"; then
+        return
+    fi
+    local profile=$HOME/.bash_profile
+    [[ -f $profile ]] || profile=$HOME/.profile
+    echo 'export PATH=$HOME/.local/bin:$PATH # Added by taskrc-kit-setup.sh' >> ${profile} || die 202
+    echo "~/.local/bin added to your PATH." >&2
+    reload_reqd=true
+}
+
+shrc_fixup() {
+    # We must ensure that .bashrc sources our settings script and that the
+    # loader symlink is present
+    [[ -e ${HOME}/.taskrc-kit-loader ]] || command ln -s ${HOME}/.local/bin/taskrc-kit/taskrc_loader ${HOME}/.taskrc-kit-loader
+    (
+        echo '[[ -n $PS1 && -e ${HOME}/.taskrc-kit-loader ]] && source ${HOME}/.taskrc-kit-loader # Added by taskrc-kit setup.sh'
+        echo
+    ) >> ${HOME}/.bashrc
+    reload_reqd=true
+}
+
+
+main() {
+    reload_reqd=false
+    if [[ ! -d $HOME/.local/bin/taskrc-kit ]]; then
+        if [[ -e $HOME/.local/bin/taskrc-kit ]]; then
+            die "$HOME/.local/bin/taskrc-kit exists but is not a directory.  Refusing to overwrite"
+        fi
+        command mkdir -p $HOME/.local/bin/taskrc-kit || die "Failed creating $HOME/.local/bin/taskrc-kit"
+    fi
+    if [[ $(inode $Script) -eq $(inode ${HOME}/.local/bin/taskrc-kit/setup.sh) ]]; then
+        die "cannot run setup.sh from ${HOME}/.local/bin"
+    fi
+    builtin cd ${HOME}/.local/bin/taskrc-kit || die "101"
+    command rm -rf ./* || die "102"
+    [[ -d ${Scriptdir} ]] || die "bad Scriptdir [$Scriptdir]"
+    command cp -r ${Scriptdir}/* ./ || die "failed copying from ${Scriptdir} to $PWD"
+    builtin cd .. # Now we're in .local/bin
+    command ln -sf ./taskrc-kit/taskrc-kit-version.sh ./
+    path_fixup "$PWD" || die "102"
+    shrc_fixup || die "104"
+    $reload_reqd && builtin echo "Shell reload required ('bash -l')" >&2
+}
+
+[[ -z $sourceMe ]] && main "$@"
--- bin/taskrc.sh
+++ bin/taskrc.sh
@@ -6,4 +6,4 @@ mydir="$(readlink -f $(dirname $0)/)"
 source "${mydir}/taskrc_loader"
 
 [[ -z $1 ]] && { echo "ERROR: at least one argument required" >&2; exit 1 ; }
-taskrc_v3 $@
+taskrc_v4 $@
