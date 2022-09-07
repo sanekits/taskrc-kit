e4ae697fd366f2b96e731292a2471ce35fb72c34
Les Matheson <les.matheson@gmail.com>
2022-09-07 07:12:54 -0400

    Sync (auto)

--- bin/templates/taskrc-template.mk
+++ bin/templates/taskrc-template.mk
@@ -6,19 +6,21 @@
 
 
 # See https://stackoverflow.com/a/73509979/237059
-absdir:=$(taskrc_dir)
+absdir := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 
-REMAKE := $(MAKE) -f $(lastword $(MAKEFILE_LIST))
+REMAKE := $(MAKE) -C $(absdir) -s -f $(lastword $(MAKEFILE_LIST))
 
 .PHONY: help
 help:
 	@echo "Targets in $(basename $(lastword $(MAKEFILE_LIST))):" >&2
-	@$(REMAKE) -s --print-data-base --question no-such-target 2>/dev/null | \
+	@$(REMAKE) --print-data-base --question no-such-target 2>/dev/null | \
 	grep -Ev  -e 'taskrc.mk' -e '^help' -e '^(Makefile|GNUmakefile|makefile|no-such-target)' | \
 	awk '/^[^.%][-A-Za-z0-9_]*:/ \
 			{ print substr($$1, 1, length($$1)-1) }' | \
 	sort | \
 	pr --omit-pagination --width=100 --columns=3
+	@echo "absdir=$(absdir)"
+	@echo "CURDIR=$(CURDIR)"
 
 .PHONY: my-target-1
 my-target-1:
