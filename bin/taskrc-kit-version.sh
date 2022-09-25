adfb19ba48f2d07653d53ea2a8145dd768c61071
Les Matheson <lmatheson4@bloomberg.net>
2022-09-25 08:39:14 -0400

    Bump to 2.0.3

--- bin/taskrc-kit-version.sh
+++ bin/taskrc-kit-version.sh
@@ -2,7 +2,7 @@
 
 # Running taskrc-kit-version.sh is the correct way to
 # get the home install path for the tool
-KitVersion=2.0.2
+KitVersion=2.0.3
 
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
