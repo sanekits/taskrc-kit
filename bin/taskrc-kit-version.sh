c29b1b76666af9f9c6efacf3eecad31b0fc0d500
Les Matheson <lmatheson4@bloomberg.net>
2022-12-05 18:25:11 -0500

    Bump for new shellkit

--- bin/taskrc-kit-version.sh
+++ bin/taskrc-kit-version.sh
@@ -2,7 +2,7 @@
 
 # Running taskrc-kit-version.sh is the correct way to
 # get the home install path for the tool
-KitVersion=2.0.5
+KitVersion=2.0.6
 
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
