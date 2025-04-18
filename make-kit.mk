ad3a9e8477909f399248e4438f270c273f2e5e4f
LES MATHESON <LMATHESON4@bloomberg.net>
2025-04-18 14:31:27 +0000

    Reconcile template v2

--- make-kit.mk
+++ make-kit.mk
@@ -3,6 +3,13 @@
 #  It defines values that are kit-specific.
 #  You should edit it and keep it source-controlled.
 
+# The shellkit/ tooling naturally evolves out from under the dependent kits.  ShellkitSetupVers allows
+# detecting the need for refresh of templates/* derived files.  To bump the root version, 
+# zap all templates/* containing 'ShellkitTemplateVers' constants and changes to the corresponding dependent kits
+# Note that within templates/* there may be diverse versions in upstream shellkit, they don't all have to match,
+# but the derived copies should be sync'ed with upstream as needed.
+ShellkitTemplateVers=2
+
 # TODO: update kit_depends to include anything which
 #   might require the kit version to change as seen
 #   by the user -- i.e. the files that get installed,
@@ -14,7 +21,7 @@ kit_depends := \
 	bin/taskrc_help \
 	bin/taskrc_new \
 
-.PHONY: publish
+.PHONY: publish publish-draft
 
 pre-publish: conformity-check
 
@@ -22,3 +29,6 @@ publish: pre-publish publish-common release-upload release-list
 	cat tmp/draft-url
 	@echo ">>>> publish complete OK. (FINAL)  <<<"
 
+publish-draft: pre-publish publish-common release-draft-upload release-list
+	cat tmp/draft-url
+	@echo ">>>> publish complete OK. (DRAFT - you must manually publish it from github release page)  <<<"
